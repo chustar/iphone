6139a5aa97247748f022744600c88fc11cfa99f3
chustar <chustar@Chumas-MacBook-Pro.local>
2015-02-14 11:25:59 -0800

    Fix downloader

--- Tinder/Tinder/TinderViewController.swift
+++ Tinder/Tinder/TinderViewController.swift
@@ -26,69 +26,109 @@ class TinderViewController: UIViewController {
             }
         }
 
-        /*
         func addPerson(urlString: String) {
             let urlRequest: NSURLRequest = NSURLRequest(URL: NSURL(string: urlString)!)
             NSURLConnection.sendAsynchronousRequest(urlRequest, queue: NSOperationQueue.mainQueue(), completionHandler: {
                 response, data, error in
+
+                var image = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+                image.image = UIImage(data: data)
+                self.view.addSubview(image)
 
                 var newUser = PFUser()
                 newUser["image"] = data
                 newUser["gender"] = rand() % 2 == 0 ? "male" : "female"
                 newUser["username"] = "user_\(rand() % 100)"
                 newUser.password = "password"
+
                 var location = PFGeoPoint(latitude: Double(37 + rand() % 20), longitude: Double(-122 + rand() % 20))
                 newUser["location"] = location
+
+                if let match = urlString.rangeOfString("twitter/(.*)128\\.jpg", options: .RegularExpressionSearch) {
+                    let index: String.Index = advance(urlString.startIndex, 8)
+                    newUser["name"] = urlString.substringWithRange(match).stringByDeletingLastPathComponent.substringFromIndex(index)
+                }
+
                 newUser.signUp()
             })
         }
 
-        addPerson("https://3.googleusercontent.com/-Nw_kkLgPSTU/AAAAAAAAAAI/AAAAAAAAIro/UcKx1NcaXvY/s160-c-k-no/photo.jpg")
-        addPerson("https://5.googleusercontent.com/-7o4IH7LO1_0/AAAAAAAAAAI/AAAAAAAAAAA/96TwdZGmqGs/s160-c-k-no/photo.jpg")
-        addPerson("https://5.googleusercontent.com/-XI4jZo-OFaw/AAAAAAAAAAI/AAAAAAAAAZs/5f9lmMVwTug/s160-c-k-no/photo.jpg")
-        addPerson("https://3.googleusercontent.com/-CjDC-WSFsTU/AAAAAAAAAAI/AAAAAAAAA9M/_mFTZE-w2F0/s160-c-k-no/photo.jpg")
-        addPerson("https://3.googleusercontent.com/-XUnyaVoreT8/AAAAAAAAAAI/AAAAAAAAAAA/YXRzlCifPKM/s160-c-k-no/photo.jpg")
-        addPerson("https://6.googleusercontent.com/-JeQ7DzreBgU/AAAAAAAAAAI/AAAAAAAAAMc/FTS2z82Y0PU/s160-c-k-no/photo.jpg")
-        addPerson("https://5.googleusercontent.com/-XgdRr6PdTyo/AAAAAAAAAAI/AAAAAAAAAGs/nM643SLImjU/s160-c-k-no/photo.jpg")
-        addPerson("https://3.googleusercontent.com/-C5TrsIqCU2A/AAAAAAAAAAI/AAAAAAAAC7o/DTQdthyDiaM/s160-c-k-no/photo.jpg")
-        addPerson("https://6.googleusercontent.com/-H-oD42VkNNo/AAAAAAAAAAI/AAAAAAAAAE8/7CGY-Iz5Trw/s160-c-k-no/photo.jpg")
-        addPerson("https://3.googleusercontent.com/-OzZcEvVuh4Y/AAAAAAAAAAI/AAAAAAAAAAA/zJTMLSpMODg/s160-c-k-no/photo.jpg")
-        addPerson("https://5.googleusercontent.com/-kNdgn2USofM/AAAAAAAAAAI/AAAAAAAAAPc/CqRWdM7i6mU/s160-c-k-no/photo.jpg")
-        addPerson("https://5.googleusercontent.com/-RMhQHcPzrnw/AAAAAAAAAAI/AAAAAAAACLo/kAFanjRZUm8/s160-c-k-no/photo.jpg")
-        addPerson("https://5.googleusercontent.com/-jZRoUqkmtoE/AAAAAAAAAAI/AAAAAAAAAAA/UT5KpGHK2c4/s160-c-k-no/photo.jpg")
-        addPerson("https://6.googleusercontent.com/-0B9XPsvsO24/AAAAAAAAAAI/AAAAAAAAAro/ng_J0xJLZUw/s160-c-k-no/photo.jpg")
-        */
-    }
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/9lessons/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/ManikRathee/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/_hartjeg/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/_shahedk/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/aaroni/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/abecherian/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/alexmarin/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/andrewaashen/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/ateneupopular/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/boheme/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/brad_frost/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/c_southam/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/cemshid/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/chadengle/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/csswizardry/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/dancounsell/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/dingyi/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/dustinlamont/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/eduardo_olv/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/enda/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/felipenogs/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/fran6/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/gerrenlamson/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/glif/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/gt/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/guiiipontes/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/iconfinder/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/idiot/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/iflendra/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/jadlimcaco/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/jaredfitch/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/jina/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/jollynutlet/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/joshhemsley/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/jsa/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/kastov_yury/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/kerem/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/kevin_granger/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/kurafire/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/leemunroe/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/mattchevy/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/mattsince87/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/mghoz/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/mizko/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/mlane/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/motherfuton/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/mrjohnwalker/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/nexy_dre/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/peterlandt/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/peterme/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/pinceladasdaweb/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/pixeliris/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/putorti/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/rem/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/rogie/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/rssems/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/sachagreif/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/shalt0ni/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/sillyleo/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/sindresorhus/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/soffes/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/suprb/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/thrivesolo/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/tomaslau/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/tonypeterson/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/tsu_vip/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/vista/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/vladabazhan/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg")
+        addPerson("https://s3.amazonaws.com/uifaces/faces/twitter/whale/128.jpg")
 
-    func wasDragged(gesture: UIPanGestureRecognizer) {
-        var label = gesture.view!
-
-        let translation = gesture.translationInView(self.view)
-        label.center = CGPoint(x: label.center.x + translation.x, y: label.center.y + translation.y)
-        gesture.setTranslation(CGPointZero, inView: self.view)
-
-        xFromCenter += translation.x
-        println(xFromCenter)
-
-        var rotation: CGAffineTransform = CGAffineTransformMakeRotation(xFromCenter / 200)
-        var scaleAmount = min(50/abs(xFromCenter), 1)
-        var scale: CGAffineTransform = CGAffineTransformScale(rotation, scaleAmount, scaleAmount)
-        label.transform = scale
-
-        if label.center.x < 100 {
-            println("Not chosen")
-        } else if label.center.x > self.view.bounds.width - 100 {
-            println("Chosen")
-        }
-
-        if gesture.state == UIGestureRecognizerState.Ended {
-            xFromCenter = 0
-            label.center = CGPointMake(self.view.bounds.width/2, self.view.bounds.height/2)
-            rotation = CGAffineTransformMakeRotation(0)
-            scaleAmount = 1
-            scale = CGAffineTransformScale(rotation, scaleAmount, scaleAmount)
-            label.transform = scale
-        }
     }
 
     override func didReceiveMemoryWarning() {
